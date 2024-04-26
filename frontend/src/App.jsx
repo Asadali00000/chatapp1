e81dcfa4561f56237d96a9c7be954f234543d755
Asad ali <80456460+Asadali00000@users.noreply.github.com>
2024-04-26 19:51:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import {  useRecoilValue } from "recoil";
 import { authAtom } from "./context/atom/Atom";
-import Toaster from 'react-hot-toast'
+
 
 // import { SocketAtomInitialise } from "./context/atom/socketAtom";
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center' >
       <Routes>
-        <Route path="/" element={userExist?<Home/>:<Navigate to={'/login'}/> }   />
+        <Route path="/" element={userExist?<Home/>:<Navigate to={'/signup'}/> }   />
         <Route path="/signup" element={userExist?<Navigate to='/' />:<SignUp/>}   />
         <Route path="/login" element={userExist?<Navigate to='/'/>:<Login/>}   />
      
