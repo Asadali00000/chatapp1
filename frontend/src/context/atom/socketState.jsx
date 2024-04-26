7d5edd237fa9a169ec984ad4aa4a14f64ffd48a1
Asad ali <80456460+Asadali00000@users.noreply.github.com>
2024-04-26 23:33:26 +0530

    Deployment

--- frontend/src/context/atom/socketState.jsx
+++ frontend/src/context/atom/socketState.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("localhost:5000", {
+			const socket = io("https://chat-app-tm8q.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
