74260ecc1fdeaf176fdf73960328cdad2da8bb5e
Asad ali <80456460+Asadali00000@users.noreply.github.com>
2024-04-26 20:14:06 +0530

    Deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
 
   
   return (
+    
     <div className='p-4 h-screen flex items-center justify-center' >
       <Routes>
         <Route path="/" element={userExist?<Home/>:<Navigate to={'/signup'}/> }   />
